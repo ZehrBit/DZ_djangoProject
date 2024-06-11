64fefe26528d2c5e802d8ca4efdf82aa269a031a
Евгений Александрович Волчёк <zehrbit@gmail.com>
2024-06-11 15:31:17 +0300

    Добавил второе приложение

--- DZ_djangoProject/settings.py
+++ DZ_djangoProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'blog2.apps.Blog2Config',
 ]
 
 MIDDLEWARE = [
---
+++ blog2/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import CustomUser, Post, Comment, Project, Version
+
+admin.site.register(CustomUser)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Project)
+admin.site.register(Version)
