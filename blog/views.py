aa2fb678131ae00fe58b3702165936c0cb5624ce
Евгений Александрович Волчёк <zehrbit@gmail.com>
2024-06-18 13:45:03 +0300

    Реализовал: 1. Выгрузку постов из базы данных 2. Отображение постов на странице

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
 
 def index(request):
-    return render(request, 'blog/post.html')
+    posts = Post.objects.filter(status='published')
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
 
+    return render(request, 'blog/post.html', context={'page_obj': page_obj, 'posts': page_obj.object_list})
 
 def about(request):
     return render(request, 'blog/about.html')
